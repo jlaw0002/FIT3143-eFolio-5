884ca16fff99e0f895d9cb213ec7227488301b33
jlaw0002 <jlaw0002@student.monash.edu>
2020-08-27 15:28:10 +1000

    Fixed up comments and header files for week4

--- Week4/week4.c
+++ Week4/week4.c
@@ -1,51 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <time.h>  
 
+//Macro defintions 
 #define MAX_GENERATED_NUM 25
 #define NUMBER_OF_SLOTS 10
 
 // Function Declarations
-int* initialiseCounter(int num);
+int* initialiseCounter();
 int findWins(int *array);
 
 int main(){
    
     int num, i;
-    int* resultsArray = initialiseCounter(MAX_GENERATED_NUM);
-   
+    int results[NUMBER_OF_SLOTS];
+    int *resultsArray=results;
+
+    
     /* Intializes random number generator */
     time_t t;
     srand((unsigned) time(&t));
         
     
+    //Parallel part of code to generate random numbers
     #pragma omp parallel for private(i, num) shared(resultsArray) schedule(static, 2) num_threads(4)
 	    for(i = 0; i < NUMBER_OF_SLOTS; i++){
 		    // Call RNG function
 		    num = (rand() % MAX_GENERATED_NUM) + 1;
-		    resultsArray[num] += 1;
+		    resultsArray[i]=num;
+
 	    }
 	
+	//Call functions to calculate wins and print result
 	printf("Number of wins: %d\n", findWins(resultsArray));
 	return 0;
 	
 }
 
-int* initialiseCounter(int num){
+int* initialiseCounter(){
+    //Create an array to store occurrences of all possible numbers
     static int array[MAX_GENERATED_NUM];
     
-    for(int i = 0; i < num; i++){
+    //Intialise each count to 0
+    for(int i = 0; i < MAX_GENERATED_NUM; i++){
         array[i] = 0;
     }
 
     return array;
 }
 
-int findWins(int *array){
+int findWins(int *resultsArray){
+    int* countArray = initialiseCounter();
     int counter = 0;
+    int num;
     
+    //Count the number of occurrences of each number in resultsArray
+    for (int i=0; i<NUMBER_OF_SLOTS;i++){
+        num = resultsArray[i];
+        countArray[num]+=1;
+    }
+    
+    //Check if occurrences of each number is greater than 1
     for(int i = 0; i < MAX_GENERATED_NUM; i++){
-        if(array[i] > 1){
+        if(countArray[i] > 1){
             counter++;
         }
     }
